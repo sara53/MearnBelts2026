3160dbf5365f4d6f03e6ec31b5b2ef72be88f440
sara <sara.atef001@gmail.com>
2025-08-26 11:18:37 +0300

    Day01

---
+++ JS/Day01/JS/demo.js
@@ -0,0 +1,24 @@
+// var fname = prompt("Enter Your Name");
+
+// console.log(fname);
+
+// var num1 = Number(prompt("enter num 1", 20));
+// var num2 = Number(prompt("enter num 2", 30));
+
+// console.log(num1 + num2);
+
+// var i = 1;
+// document.writeln("<h" + i + ">Hello at iti</h" + i + ">");
+
+// var result = confirm("Are you sure you want to buy this course");
+
+// // ternary operator
+// result
+// 	? document.writeln("<h1 class='success'>Course added to your list</h1>")
+// 	: document.writeln("<h1 class='error'>Operation cancelled</h1>");
+
+// var color = "red";
+
+// color == "red" ? alert("Fav color is red") : console.log("No Color detected");
+
+// alert("hello mearn at iti");
---
+++ JS/Day01/main.js
@@ -0,0 +1,179 @@
+// var x = 3;
+
+// var flag = true;
+
+// console.log(fname);
+
+// console.log(x);
+
+// console.log(flag);
+
+// /**
+//  * first
+//  * firstSecond
+//  * firstSecondThird
+//  */
+// var firstName = "ali"; //string
+// var num = 10; // int
+
+// var num2 = 10.1; // float - double
+// var c = "h"; // char
+
+// console.log(typeof firstName);
+// console.log(typeof num);
+
+// console.log(typeof num2);
+// console.log(typeof c);
+// var fname = "ali";
+// // var fname = 10;
+// // fname = "alaa";
+
+// // fname = 10;
+// // fname = true;
+
+// console.log(fname);
+
+// var student = null;
+// console.log(student);
+// console.log(typeof student);
+// var fname;
+// console.log(fname);
+// fname = "alaa";
+// console.log(typeof fname);
+
+// var x = 10;
+
+// console.log(typeof x);
+
+// x = "alaa";
+
+// console.log(typeof x);
+// CamelCase
+// jsEngine -- V8
+/**
+ * Datatypes
+ * -----------------------------------------
+ * == Primitive datatype
+ * -----------------------------------------
+ * == string
+ * == number
+ * == boolean
+ * == undefined
+ * == null
+ * --------------------Reference Datatype-----------------------
+ * == object
+ *--------------(var keyWord)----------------
+    == reassign with deferent datatypes
+    == allow redeclaration
+ */
+
+// var x = 10;
+// console.log(typeof x); //number
+
+// coercion -- js engine can convert from datatype to anther datatype automatically
+// var fname = "ali"; // literal creation
+// var fname = String(10);
+
+// console.log(fname);
+// console.log(typeof fname);
+
+/**================================= */
+// NaN -- Not a Number
+// var x = Number("mona");
+
+// console.log(x);
+// console.log(typeof x);
+/**===========================
+ * == falsy Values
+ * ---------------------------
+ *  == false
+ *  == 0
+ *  == NaN
+ *  == null
+ *  == undefined
+ *  == ""
+ * --------------------------------
+ *  == truthy Values
+ * ---------------------------
+ * ================= */
+// var x = Boolean("sdsfg");
+
+// console.log(x);
+
+// var x = "true"; // number NaN
+// var y = true; // number - 1
+
+// console.log(x == y);
+
+// var x = "true";
+// var y = true;
+// console.log(x == y);
+// console.log(x === y);
+
+// var x = "10"; // 10
+
+// var y = 10; // number
+
+// console.log(x - y);
+// var x = 10; // string
+// var y = "20";
+
+// console.log(x + y);
+// var x = "Hello";
+
+// var y = "at ITI";
+
+// console.log(x + y);
+/** Operators
+ * -------------------------
+ *  [+]
+ *      number + number = add
+ *      string + string = concat
+ * ------------------------------------
+ *  [==]   -- (values)
+ *  [===]  -- (values &datatypes)
+ */
+
+/**
+ * and
+ * -----------------------
+ *  == true && true === true
+ *  == true && false === false
+ *  == false && true === false
+ *  == false && false === false
+ * -------------------------------------
+ * or
+ * -----------------------
+ *  == true || true === true
+ *  == true || false === true
+ *  == false || true === true
+ *  == false || false === false
+ */
+
+/**
+ *  "mona" && "ali"
+ *  true && == ali
+ *
+ *  0 && 11
+ * -------------------
+ *  "mona" || "ali" --
+ *
+ * 5 || undefined
+ * undefined || ""
+ *
+ */
+// for (var i = 0; i < 5; i++) {
+// 	console.log(i);
+// }
+
+// var i = 0;
+// while (i < 5) {
+// 	i++;
+// 	console.log(i);
+// }
+
+// var i = 9;
+// do {
+// 	i++;
+// 	console.log(i);
+// } while (i < 5);
