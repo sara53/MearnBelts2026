52b0ea30ce937e1c40cb501b96fe75bb1c2fd0bf
sara <sara.atef001@gmail.com>
2025-08-27 10:12:37 +0300

    day02 Part1

---
+++ JS/Day02/main.js
@@ -0,0 +1,123 @@
+// function sum(x, y) {
+// 	console.log("x : ", x);
+// 	console.log("y : ", y);
+// 	console.log(x + y);
+// 	console.log("----------------");
+// }
+
+// sum("mona", "Ali");
+// sum(true, true);
+// sum(10, 20);
+// sum();
+// sum(10);
+// sum(10, 20);
+// sum(10, 20, 40, 50);
+// arguments
+// sum(3, 4);
+// console.log("----------");
+// sum(2, 5);
+// console.log("-------------");
+
+// sum(5, 6);
+// console.log(sum(3, 4));
+// console.log(sum(4, 5));
+// var result = sum(2, 3);
+
+// console.log(result);
+
+// console.log(result);
+// var result = Number("10");
+
+// console.log(result);
+// function statement
+// function sum(x, y) {
+// 	console.log("x : ", x);
+// 	console.log("y : ", y);
+// }
+// function expression
+
+// var result = function (x, y) {
+// 	console.log(x + y);
+
+// 	return "ITI";
+// };
+
+// console.log(result(3, 4));
+//
+
+// console.log(fname);
+// // var fname = "ali";
+// console.log(fname);
+
+// var fname;
+// console.log(fname);
+// fname = "ali";
+// console.log(fname);
+// sum(3, 4);
+// function sum(x, y) {
+// 	console.log(x + y);
+// }
+
+// sum(3, 4);
+// console.log(sum);
+// var sum = function (x, y) {
+// 	console.log(x + y);
+// };
+
+// great("maod");
+// callback function
+// great(dotnet);
+// console.log("-----------");
+// great(mearn);
+//
+
+// function great(myFun) {
+// 	myFun();
+// 	console.log("Welcome at ITI");
+// }
+
+// // great(function () {
+// // 	console.log("Welcome Python at iti");
+// // });
+// // console.log("----------");
+// // great(function () {
+// // 	console.log("Hello mearn at iti");
+// // });
+// var mearn = function (instuctor) {
+// 	console.log("Hello mearn at iti");
+// 	console.log("Your inst is ,", instuctor);
+// };
+
+// function great(myFun, insName) {
+// 	myFun(insName);
+// 	console.log("Welcome at ITI");
+// }
+
+// great(mearn, "ahmed");
+
+// console.log("--------------");
+
+// great(function (ins) {
+// 	console.log("Hello Dotnet at iti");
+// 	console.log("Your inst is ,", ins);
+// }, "Ali");
+// function great(myFun1, myFun2) {
+// 	myFun1(function () {
+// 		console.log("inner fun");
+// 	});
+// 	console.log("------------------");
+// 	myFun2(function () {
+// 		console.log("inner fun 2");
+// 	});
+// }
+
+// great(
+// 	function (x) {
+// 		x();
+// 		console.log("Cb 1");
+// 	},
+// 	function (y) {
+// 		y();
+// 		console.log("CB 2");
+// 	}
+// );
